54766d09e99ce30b8459f0bbe1e6fb2a7c71f396
helloyeew <gameforphawit@outlook.com>
2020-12-24 18:28:17 +0700

    ✅ Delete Final files
    I'm scared 😱

--- Final ComPro 1/RealFinal/final_supaporn/stock.py
+++
@@ -1,8 +0,0 @@
-class Stock:
-    def __init__(self,item,amount,price):
-        self.item = item
-        self.amount = amount
-        self.price = price
-
-    def __str__(self):
-        return (f"({self.item},{self.amount},{self.price})")
--- Final ComPro 1/RealFinal/supaporn/final_spe/order.py
+++
@@ -1,9 +0,0 @@
-class Order:
-    def __init__(self,item,amount):
-        self.item = item
-        self.status = "To process"
-        self.amount = amount
-        self.cost = 0
-
-    def __str__(self):
-        return f"({self.item},{self.amount},{self.cost} Baht,{self.status})"
